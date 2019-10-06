de29177191276aa46ec4fd4f2cb90932ced71c28
nwheatle <wheatley.nm@gmail.com>
2019-10-06 15:32:53 -0700

    added button emitter signal still doesn't work

--- server.js
+++ server.js
@@ -9,11 +9,15 @@ app.set("views", "./public/views");
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "/public/")));
 
+io.on("connection", function(socket) {
+  console.log("connection made to server");
+});
+
+io.on("signal", function() {
+  console.log("How to make this execute on 'signal'?");
+});
+
 app.get("/", function(req, res) {
   console.log("GET request success");
-
-  io.on("connection", function(socket) {
-    console.log("connection made to server");
-  });
   res.render("view");
 });
